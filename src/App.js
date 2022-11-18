d17a2d4026cfea04f587899e1b7ef881d6d7ca15
Josh <joshua.developer21@gmail.com>
2022-11-18 11:19:42 -0500

    first commit shiftlabor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { Form, Button, Modal, NavItem } from "react-bootstrap";
+import { useState } from "react";
 
 function App() {
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [date, setDate] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleClose = (e) => {
+    setShow(false);
+  };
+  const sendDataButton = (e) => {
+    e.preventDefault();
+    setShow(true);
+  };
+
+  function sendData(e) {
+    const data = {
+      name: name,
+      lastName: lastName,
+      date: date,
+      message: message,
+    };
+    console.log(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Form className="m-3" onClick={sendData}>
+        <Form.Group controlId="formName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="formLastName">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Apellido"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Fecha de Nacimiento</Form.Label>
+          <Form.Control
+            type="date"
+            placeholder="Ingresar fecha de nacimiento"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="formArea">
+          <Form.Label>Mensaje Pesonalizado</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows="6"
+            placeholder="Mensaje"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </Form.Group>
+        <Button
+          variant="primary"
+          type="submit"
+          className="mt-3"
+          onClick={sendDataButton}
         >
-          Learn React
-        </a>
-      </header>
+          ENVIAR
+        </Button>
+      </Form>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <h3>USUARIO REGISTRADO</h3>
+        </Modal.Header>
+        <Modal.Body className="text-center">
+          <div>
+            <p>Nombre: {name}</p>
+            <p>Apellidos: {lastName}</p>
+            <p>Fecha de Nacimiento: {date}</p>
+            <p>Mensaje: {message}</p>
+            <h5>BIENVENIDO</h5>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
